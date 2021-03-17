1bfa1b8cf47e152cc4b04d96a8dfbdca9b7791d3
dockerlead <dockerlead@mail.com>
2021-03-17 00:06:46 -0500

    Sync template and ts form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null),
+      'email': new FormControl(null),
+      'projectStatus': new FormControl('Stable')
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.projectForm);
+  }
 }
