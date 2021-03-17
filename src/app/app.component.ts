6b948d72bb5a6913c424d409e7fe3714e3f1b427
dockerlead <dockerlead@mail.com>
2021-03-17 00:27:14 -0500

    Add custom async validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, [Validators.required, this.validateProjectNameSync.bind(this)]),
+      'projectName': new FormControl(null, [Validators.required, this.validateProjectNameSync.bind(this)], this.validateProjectNameAsync),
       'email': new FormControl(null, Validators.email),
       'projectStatus': new FormControl('Stable')
     });
@@ -27,5 +28,18 @@ export class AppComponent implements OnInit {
       return {'projectNameIsForbidden': true};
     }
     return null;
+  }
+
+  validateProjectNameAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Joke') {
+          resolve({'projectNameIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+    return promise;
   }
 }
